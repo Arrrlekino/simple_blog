a64e80aae857466682db4c45ef1b8827348b6c1f
Alla Massorova <zebrasteel@gmail.com>
2020-04-20 15:53:48 +0200

    Added articles#edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,14 @@
 class ArticlesController < ApplicationController
-	def new 
-	end	
+		
 	def index
 		@articles = Article.all
 	end	
 	def show
 		@article = Article.find(params[:id])
 	end	
+
+	def new 
+	end
 
 	def create
 		#render plain: params[:article].inspect
@@ -20,6 +22,20 @@ class ArticlesController < ApplicationController
 	end
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		
+		if @article.update(article_params)
+		redirect_to @article
+	else
+		render action: 'edit'
+	end
+	end
+		
 	private
 
 	def article_params
